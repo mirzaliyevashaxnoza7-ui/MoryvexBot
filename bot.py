696a6fb62a2dcc68b4c82b6058377aa4689b2bde
mirzaliyevashaxnoza7-ui <mirzaliyevashaxnoza7@gmail.com>
2026-08-18 14:25:51 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,28 +9,6 @@ from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandl
 TOKEN = "8737182258:AAHMlj4Xzym8svHvC4YLANw9JQ3kADE-b4Y"
 
 ADMIN_ID = 8319293537
-async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_user.id != ADMIN_ID:
-        return
-
-    cursor.execute("SELECT COUNT(*) FROM users")
-    total_users = cursor.fetchone()[0]
-
-    await update.message.reply_text(
-        "👑 ADMIN PANEL\n\n"
-        f"👥 Jami foydalanuvchilar: {total_users} ta"
-    )
-async def admin(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    if update.effective_user.id != ADMIN_ID:
-        return
-
-    cursor.execute("SELECT COUNT(*) FROM users")
-    total = cursor.fetchone()[0]
-
-    await update.message.reply_text(
-        f"👑 ADMIN PANEL\n\n"
-        f"👥 Jami foydalanuvchilar: {total} ta"
-    )
 
 BOT_USERNAME = "Kasetachi_uz_bot"
 
@@ -436,13 +414,6 @@ def main():
             buttons
         )
     )
-    app.add_handler(
-    CommandHandler("start", start)
-)
-
-app.add_handler(
-    CommandHandler("admin", admin)
-)
     print("🤖 KASETACHI BOT ISHLAYAPTI!")
 
     app.run_polling()
